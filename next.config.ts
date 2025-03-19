707835abcdff28c52a56e8aac3916c4f25eea07e
Olga Vasilyeva <olga.vasilyeva@ispring.lan>
2025-03-19 23:14:27 +0300

    попытка запустить

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,4 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
 module.exports = {
-    trailingSlash: true,  // Для работы с GitHub Pages
-    output: 'export', // Это указывает Next.js, что вы хотите экспортировать статический сайт.
-}
-
-export default nextConfig;
+    trailingSlash: true,
+    output: 'export',
+};
